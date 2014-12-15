3b15eee71bdfc075b2d2dafd118c3fa3d0c16f0e
Pascal Drewes <pascal@drewes-webdesign.nl>
2014-12-15 19:17:35 +0100

    testing the update function

--- index.php
+++ index.php
@@ -5,13 +5,10 @@ error_reporting(E_ALL);
 require_once 'classes/db.class.php';
 $db = new db();
 $db->db_table = "DEBUG";
-$data = array(
-    "msg1" => "this is weird",
-);
+$fields = array('id', 'msg1');
 $where = array(
-    "id" => "2",
-    "msg" => "2"
+    "id" => "2"
 );
 echo "<pre>";
-var_dump($db->update($data, $where));
+var_dump($db->select($fields, $where));
 echo "</pre>";
