987749ce9ad84e7aa65b71f62ab9416d9700e568
Pascal Drewes <pascal@drewes-webdesign.nl>
2014-12-15 18:01:11 +0100

    forgot semicolon

--- classes/db.class.php
+++ classes/db.class.php
@@ -11,7 +11,7 @@ class db {
 
     public function __construct() {
         try {
-            $link = new PDO('mysql:host=' . $this->db_host . 'dbname=' . $this->db_name . 'charset=utf8', $this->db_user, $this->db_pass);
+            $link = new PDO('mysql:host=' . $this->db_host . ';dbname=' . $this->db_name . ';charset=utf8', $this->db_user, $this->db_pass);
             $link->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             $link->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
         } catch (PDOException $e) {
