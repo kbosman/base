eed69cbc7d7d7739fba41edd50a88dec7f9ea23d
Pascal Drewes <pascal@drewes-webdesign.nl>
2014-12-15 16:53:20 +0100

    testing prepared statement

--- classes/db.class.php
+++ classes/db.class.php
@@ -18,28 +18,61 @@ class db {
 
     public function insert($array) {
         //note: $array should use the keys as the field names! example: array(id => 1, name => john)
-        $sql = "INSERT INTO " . $this->db_table;
-        $fields = "(";
-        $values = " VALUES (";
-        $last_field = key(array_slice($array, -1, 1, TRUE));
-        $last_value = '"' . array_pop($array) . '"';
-        foreach ($array as $key => $value) {
-            $fields .= $key . ", ";
-            $values .= '"' . $this->link->real_escape_string($value) . '", ';
+//        $sql = "INSERT INTO " . $this->db_table;
+//        $fields = "(";
+//        $values = " VALUES (";
+//        $last_field = $this->link->real_escape_string(key(array_slice($array, -1, 1, TRUE)));
+//        $last_value = '"' . $this->link->real_escape_string(array_pop($array)) . '"';
+//        foreach ($array as $key => $value) {
+//            $fields .= $this->link->real_escape_string($key) . ", ";
+//            $values .= '"' . $this->link->real_escape_string($value) . '", ';
+//        }
+//        $fields .= $last_field . ")";
+//        $values .= $last_value . ")";
+//        $sql .= $fields . $values;
+//        $query = $this->link->query($sql);
+//        if ($query) {
+//            return TRUE;
+//        } else {
+//            DIE("Error on insert: ('" . $this->link->errno . "' -> " . $this->link->error . ") SQL: " . $sql);
+//            return FALSE;
+//        }
+        $insert = array();
+        foreach($array as $t) $types .= "s";
+        $insert[] = &$types;
+        foreach($array as $key => $value){
+            $insert[] = &$value;
         }
-        $fields .= $last_field . ")";
-        $values .= $last_value . ")";
-        $sql .= $fields . $values;
-        $query = $this->link->query($sql);
-        if($query){
-            return TRUE;
-        } else {
-            DIE("Error on insert: ('" . $this->link->errno . "' -> " . $this->link->error . ") SQL: " . $sql);
-            return FALSE;
-        }
+        return $insert;
     }
 
-    public function update() {
+    public function update($data, $where) {
+        //$data and $where should be a associates array with the field name as the key
+        $sql = "UPDATE " . $this->db_table . " SET (";
+
+        // getting last field and value because they shouldn't have a , at the end
+        $last = '"' . $this->link->real_escape_string(key(array_slice($data, -1, 1, TRUE))) . '" = "' . $this->link->real_escape_string(array_pop($data)) . '"';
+        
+        // data processing
+        foreach ($data as $key => $value) {
+            $sql .= '"' . $this->link->real_escape_string($key) . '" = "' . $this->link->real_escape_string($value) . '", ';
+        }
+        
+        // adding the last fields and where clause to the sql
+        $sql .= $last . ") WHERE ";
+        
+        // getting last field and value because they shouldn't have a , at the end
+        $last = '"' . $this->link->real_escape_string(key(array_slice($where, -1, 1, TRUE))) . '" = "' . $this->link->real_escape_string(array_pop($where)) . '"';
+        
+        // data processing
+        foreach ($where as $key => $value) {
+            $sql .= '"' . $this->link->real_escape_string($key) . '" = "' . $this->link->real_escape_string($value) . '", ';
+        }
+        
+        // and the last assembly
+        $sql .= $last . ";";
+        
+        // execute the query
         
     }
 
