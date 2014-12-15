142d046d81a7e72da78a1b2bae21f33caa6003c3
Pascal Drewes <pascal@drewes-webdesign.nl>
2014-12-15 15:51:06 +0100

    db was not selected

--- classes/db.class.php
+++ classes/db.class.php
@@ -6,10 +6,11 @@ class db {
     private $db_host = "localhost";
     private $db_user = "c1stenden";
     private $db_pass = "hdzaIS#8P";
+    private $db_name = "c1base";
     public $db_table;
 
     public function __construct() {
-        $this->link = new mysqli($this->db_host, $this->db_user, $this->db_pass);
+        $this->link = new mysqli($this->db_host, $this->db_user, $this->db_pass, $this->db_name);
         if ($this->link->connect_errno) {
             die("Error connecting to the database: " . $this->link->connect_error);
         }
