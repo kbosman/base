e7f7585c1f6ee38138cc284eb23ecfb44014c38c
Pascal Drewes <pascal@drewes-webdesign.nl>
2014-12-15 13:06:50 +0100

    started on db.class

--- classes/db.class.php
+++ classes/db.class.php
@@ -1,4 +1,14 @@
 <?php
 class db {
-    //put your code here
+    
+    private $link;
+    private $db_host = "localhost";
+    private $db_user = "c1stenden";
+    private $db_pass = "hdzaIS#8P";
+    public $db_table;
+    
+    public function __construct() {
+        $this->link = new mysqli($this->host, $this->db_user, $this->db_pass, $this->)
+    }
+    
 }
