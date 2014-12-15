2843359d1635fa9000e9d6fe5a9070c6e7dbf503
Pascal Drewes <pascal@drewes-webdesign.nl>
2014-12-15 17:57:35 +0100

    testing pdo

--- classes/db.class.php
+++ classes/db.class.php
@@ -10,69 +10,25 @@ class db {
     public $db_table;
 
     public function __construct() {
-        $this->link = new mysqli($this->db_host, $this->db_user, $this->db_pass, $this->db_name);
-        if ($this->link->connect_errno) {
-            die("Error connecting to the database: " . $this->link->connect_error);
-        }
+        $link = new PDO('mysql:host=' . $this->db_host . 'dbname=' . $this->db_name . 'charset=utf8', $this->db_user, $this->db_pass);
+        $link->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $link->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
     }
 
     public function insert($array) {
         //note: $array should use the keys as the field names! example: array(id => 1, name => john)
-//        $sql = "INSERT INTO " . $this->db_table;
-//        $fields = "(";
-//        $values = " VALUES (";
-//        $last_field = $this->link->real_escape_string(key(array_slice($array, -1, 1, TRUE)));
-//        $last_value = '"' . $this->link->real_escape_string(array_pop($array)) . '"';
-//        foreach ($array as $key => $value) {
-//            $fields .= $this->link->real_escape_string($key) . ", ";
-//            $values .= '"' . $this->link->real_escape_string($value) . '", ';
-//        }
-//        $fields .= $last_field . ")";
-//        $values .= $last_value . ")";
-//        $sql .= $fields . $values;
-//        $query = $this->link->query($sql);
-//        if ($query) {
-//            return TRUE;
-//        } else {
-//            DIE("Error on insert: ('" . $this->link->errno . "' -> " . $this->link->error . ") SQL: " . $sql);
-//            return FALSE;
-//        }
-        $insert = array();
-        foreach($array as $t) $types .= "s";
-        $insert[] = &$types;
-        foreach($array as $key => $value){
-            $insert[] = &$value;
+        try {
+            $stmt = $this->link->prepare("INSERT INTO " . $this->db_table . "(:field) VALUES(:value)");
+            $stmt->bindParam(':field', array_keys($array), PDO::PARAM_STR);
+            $stmt->bindParam(':value', array_values($array), PDO::PARAM_STR);
+            $stmt->execute();
+            return $stmt->rowCount();
+        } catch (PDOException $ex) {
+            echo "Error on insert: " . $ex.getMessage();
         }
-        return $insert;
     }
 
     public function update($data, $where) {
-        //$data and $where should be a associates array with the field name as the key
-        $sql = "UPDATE " . $this->db_table . " SET (";
-
-        // getting last field and value because they shouldn't have a , at the end
-        $last = '"' . $this->link->real_escape_string(key(array_slice($data, -1, 1, TRUE))) . '" = "' . $this->link->real_escape_string(array_pop($data)) . '"';
-        
-        // data processing
-        foreach ($data as $key => $value) {
-            $sql .= '"' . $this->link->real_escape_string($key) . '" = "' . $this->link->real_escape_string($value) . '", ';
-        }
-        
-        // adding the last fields and where clause to the sql
-        $sql .= $last . ") WHERE ";
-        
-        // getting last field and value because they shouldn't have a , at the end
-        $last = '"' . $this->link->real_escape_string(key(array_slice($where, -1, 1, TRUE))) . '" = "' . $this->link->real_escape_string(array_pop($where)) . '"';
-        
-        // data processing
-        foreach ($where as $key => $value) {
-            $sql .= '"' . $this->link->real_escape_string($key) . '" = "' . $this->link->real_escape_string($value) . '", ';
-        }
-        
-        // and the last assembly
-        $sql .= $last . ";";
-        
-        // execute the query
         
     }
 
