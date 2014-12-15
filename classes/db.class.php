ceaad2ea2e049d76cb155d4be8a543fd5b3c402c
Pascal Drewes <pascal@drewes-webdesign.nl>
2014-12-15 18:31:14 +0100

    insert working useing pdo

--- classes/db.class.php
+++ classes/db.class.php
@@ -11,9 +11,9 @@ class db {
 
     public function __construct() {
         try {
-            $link = new PDO('mysql:host=' . $this->db_host . ';dbname=' . $this->db_name . ';charset=utf8', $this->db_user, $this->db_pass);
-            $link->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            $link->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+            $this->link = new PDO('mysql:host=' . $this->db_host . ';dbname=' . $this->db_name . ';charset=utf8', $this->db_user, $this->db_pass);
+            $this->link->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $this->link->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
         } catch (PDOException $e) {
             echo "Error: " . $e->getMessage();
         }
@@ -22,13 +22,22 @@ class db {
     public function insert($array) {
         //note: $array should use the keys as the field names! example: array(id => 1, name => john)
         try {
-            $stmt = $this->link->prepare("INSERT INTO " . $this->db_table . "(:field) VALUES(:value)");
-            $stmt->bindParam(':field', array_keys($array), PDO::PARAM_STR);
-            $stmt->bindParam(':value', array_values($array), PDO::PARAM_STR);
+            $sql = "INSERT INTO " . $this->db_table . " (";
+            $fields = array();
+            $params = array();
+            foreach ($array as $field => $value) {
+                $params[] = '?';
+                $fields[] = $field;
+            }
+            $stmt = $this->link->prepare($sql . join(', ', $fields) . ") VALUES (" . join(', ', $params) . ")");
+            $i = 0;
+            foreach ($array as $value){
+                $stmt->bindValue(++$i, $value);
+            }
             $stmt->execute();
             return $stmt->rowCount();
         } catch (PDOException $e) {
-            echo "Error on insert: " . $e->getMessage();
+            echo "Error on insert: " . $e->getMessage() . "<br/>";
         }
     }
 
