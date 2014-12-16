61a698ca58a2656058faa73da1f0227849c035e2
Pascal Drewes <pascal@drewes-webdesign.nl>
2014-12-16 14:33:08 +0100

    Start aan overzicht alle tickets

--- classes/output.class.php
+++ classes/output.class.php
@@ -1,30 +1,43 @@
 <?php
-class output extends db 
-{
+require_once 'db.class.php';
+class output extends db {
     
     public function __construct() {
         parent::__construct();
         $this->db_table = "";
         session_start();
     }
+
+    public function tickets($velden, $bedrijf = NULL, $periode = NULL) {
+        $sql = "SELECT " . join(", ", $velden) ." FROM TICKET, STATUS_WIJZIGING";
+        $result = $this->select(NULL, NULL, $sql);
+        // idTicket     TICKET      
+        // bedrijfsnaam BEDRIJF     STATUS_WIJZIGING.idBedrijf
+        $alleTicketID = $this->select(array("idTicket"));
+        $return = array();
+        foreach ($alleTicketID as $ticket){
+            
+        }
+    }
+    // SELECTING STUFF
+//$fields = array("msg", "msg1");
+//$where = array(
+//    "id" => "1"
+//);
+//echo "<pre>";
+//var_dump($db->select($fields, $where));
+//echo "</pre>";
+//
     
-    public function tickets()
-    {
+    public function openTickets() {
         
     }
     
-    public function openTickets()
-    {
+    public function oplostijdTickets() {
         
     }
     
-    public function oplostijdTickets()
-    {
-        
-    }
-    
-    public function ticket()
-    {
+    public function ticket() {
         
     }
 }
