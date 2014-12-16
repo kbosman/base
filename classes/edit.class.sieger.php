edb9d578fb2ef6ea5c2ea72b75416c198c5346be
SGiest <s.g.iest@hotmail.com>
2014-12-16 15:51:36 +0100

    added return

--- classes/edit.class.sieger.php
+++ classes/edit.class.sieger.php
@@ -2,24 +2,81 @@
 
 class edit
 {
-    $select = 
-    // WIJZIG GEGEVENS MEDEWERKER
+// WIJZIG GEGEVENS MEDEWERKER
     public function editMedewerker()
-    {
-        $fields = array("idMedewerker", "Gebruikersnaam", "Email" , "Voornaam", "Achternaam", "Tussenvoegsel");
-        $where = array($_POST['textveld']);    
-    }
-    //WIJZIG GEGEVENS BEDRIJFSMEDEWERKER
-    public function editBedrijfsmedewerker()
-    {
-        $fields = array("idBedrijfsMedewerkers", "idBedrijf", "Gebruikersnaam", "Email", "Voornaam", "Achternaam", "Tussenvoegsel", "Functie");
-        $where = array($_POST['textveld']);        
-    }
-    //WIJZIG GEGEVENS BEDRIJF
-    public function editBedrijf()
-    {
-        $fields = array("idBedrijf", "Bedrijfsnaam");
-        $where = array($_POST['textveld']);        
+    {   //OPHALEN GEGEVENS
+        $fields = array(
+            "idMedewerker", 
+            "Gebruikersnaam", 
+            "Email", 
+            "Voornaam", 
+            "Achternaam", 
+            "Tussenvoegsel");
+        
+        $where = array("" => "");
+        
+        //GEGEVENS TERUG VOEREN
+        return $data = array (
+            "idMedewerker" => "",
+            "Gebruikersnaam" => "",
+            "Email" => "",
+            "Voornaam" => "", 
+            "Achternaam" => "",
+            "Tussenvoegsel" => ""); 
+        
+        $where = $where;   
     }
 
+//WIJZIG GEGEVENS BEDRIJFSMEDEWERKER
+    public function editBedrijfsmedewerker()
+    {   //OPHALEN GEGEVENS
+        $fields = array(
+            "idBedrijfsMedewerkers", 
+            "idBedrijf", 
+            "Gebruikersnaam", 
+            "Email", 
+            "Voornaam", 
+            "Achternaam", 
+            "Tussenvoegsel", 
+            "Functie");
+        
+        $where = array("" => "");
+        
+        //GEGEVENS TERUG VOEREN
+        return $data = array
+            ("idBedrijfsMedewerkers" => "", 
+            "idBedrijf" => "",
+            "Gebruikersnaam" => "",
+            "Email" => "",
+            "Voornaam" => "", 
+            "Achternaam" => "", 
+            "Tussenvoegsel" => "",
+            "Functie" => "");
+        
+        $where = $where;}
+
+//WIJZIG GEGEVENS BEDRIJF
+    public function editBedrijf()
+    {   //OPHALEN GEGEVENS
+        $fields = array(
+            "idBedrijf",
+            "Bedrijfsnaam",
+            "Adresgegevens",
+            "Telefoon",
+            "Email", 
+            "Licentie");
+        
+        $where = array("" => "");
+        
+        //GEGEVENS TERUG VOEREN
+        return $data = array(
+            "idBedrijf" => "",
+            "Bedrijfsnaam" => "",
+            "Adresgegevens" => "",
+            "Telefoon" => "",
+            "Email" => "", 
+            "Licentie" => "");
+        
+        $where = $where;
+    }
 }
