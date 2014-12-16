31d4753e1efe7d9c74c1a8d08c826a93b61a7e9b
LesleyJordan <ljvanoostenrijk@live.nl>
2014-12-16 12:53:06 +0100

    Begin gemaakt voor output

--- classes/output.class.php
+++ classes/output.class.php
@@ -3,5 +3,27 @@ class output extends db {
     
     public function __construct() {
         parent::__construct();
+        $this->db_table = "";
+        session_start();
+    }
+    
+    public function tickets()
+    {
+        
+    }
+    
+    public function openTickets()
+    {
+        
+    }
+    
+    public function oplostijdTickets()
+    {
+        
+    }
+    
+    public function ticket()
+    {
+        
     }
 }
