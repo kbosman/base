4ac1d7ea6c73b4d0e0aa4a80403c5a8468070290
SGiest <s.g.iest@hotmail.com>
2014-12-16 14:33:14 +0100

    velden

--- classes/edit.class.sieger.php
+++ classes/edit.class.sieger.php
@@ -1,16 +1,25 @@
 <?php
-class edit {
-    
-    public function editMedewerker(){
-        
+
+class edit
+{
+    $select = 
+    // WIJZIG GEGEVENS MEDEWERKER
+    public function editMedewerker()
+    {
+        $fields = array("idMedewerker", "Gebruikersnaam", "Email" , "Voornaam", "Achternaam", "Tussenvoegsel");
+        $where = array($_POST['textveld']);    
     }
-    
-    public function editBedrijfsmedewerker(){
-        
+    //WIJZIG GEGEVENS BEDRIJFSMEDEWERKER
+    public function editBedrijfsmedewerker()
+    {
+        $fields = array("idBedrijfsMedewerkers", "idBedrijf", "Gebruikersnaam", "Email", "Voornaam", "Achternaam", "Tussenvoegsel", "Functie");
+        $where = array($_POST['textveld']);        
     }
-    
-    public function editBedrijf() {
-        
+    //WIJZIG GEGEVENS BEDRIJF
+    public function editBedrijf()
+    {
+        $fields = array("idBedrijf", "Bedrijfsnaam");
+        $where = array($_POST['textveld']);        
     }
-            
+
 }
