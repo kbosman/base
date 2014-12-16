dbb2c3ac72d77ec6d7a476cbb48711d7c7549a6c
Pascal Drewes <pascal@drewes-webdesign.nl>
2014-12-16 12:30:21 +0100

    demo insert

--- index.php
+++ index.php
@@ -4,7 +4,13 @@ error_reporting(E_ALL);
 require_once 'classes/db.class.php';
 $db = new db();
 $db->db_table = "DEBUG";
-$where = array("id" => "2", "msg" => "Test");
-echo "<pre>";
-var_dump($db->delete($where));
-echo "</pre>";
+
+
+//INSERT UPDATE SELECT DELETE
+
+$array = array(
+    "id" => 150,
+    "msg" => "Twitter is kut",
+    "msg1" => "vooral de foto's"
+);
+var_dump($db->insert($array));
