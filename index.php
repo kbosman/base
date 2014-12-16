5171616dee203221734f9080f3e2e4d5fdcb0c3a
Pascal Drewes <pascal@drewes-webdesign.nl>
2014-12-16 13:01:54 +0100

    added html tags

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php
-
 error_reporting(E_ALL);
 
 // DEMO ON DB.CLASS
@@ -45,23 +44,29 @@ require_once 'classes/user.class.php';
 $user = new user();
 
 
-if($_SERVER['REQUEST_METHOD'] == "POST" && isset($_POST['submit'])) {
+if ($_SERVER['REQUEST_METHOD'] == "POST" && isset($_POST['submit'])) {
     var_dump($user->register($fields));
 }
-
 ?>
-<form action="" method="post">
-    Gebruikersnaam: <input type="text" name="Gebruikersnaam" />
-    <br/>
-    Wachtwoord: <input type="text" name="Wachtwoord" />
-    <br/>
-    Autorisatie:<br/>
-    <select name="Autorisatie">
-        <option value="Teamleider">Teamleider</option>
-        <option value="Admin">Admin</option>
-        <option value="Medewerker">Medewerker</option>
-        <option value="Bedrijfsmedewerker">Bedrijfsmedewerker</option>
-    </select>
-    <br/>
-    <input type="submit" value="submit" name="submit" />
-</form>
+<html>
+    <head>
+        <title>Test</title>
+    </head>
+    <body>
+        <form action="" method="post">
+            Gebruikersnaam: <input type="text" name="Gebruikersnaam" />
+            <br/>
+            Wachtwoord: <input type="text" name="Wachtwoord" />
+            <br/>
+            Autorisatie:<br/>
+            <select name="Autorisatie">
+                <option value="Teamleider">Teamleider</option>
+                <option value="Admin">Admin</option>
+                <option value="Medewerker">Medewerker</option>
+                <option value="Bedrijfsmedewerker">Bedrijfsmedewerker</option>
+            </select>
+            <br/>
+            <input type="submit" value="submit" name="submit" />
+        </form>
+    </body>
+</html>
