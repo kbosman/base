6bd4c352d6d84c5dab1ab818ce06811087c05420
martijn13795 <martijn13795@hotmail.com>
2014-12-16 12:17:08 +0100

    tiding up

--- index.php
+++ index.php
@@ -1,12 +1,5 @@
 <?php
-
-<<<<<<< HEAD
-echo "<h1>Hello world</h1>";
-echo "Hallo, dit is een test van Lesley";
-echo "banaan"
-=======
 error_reporting(E_ALL);
->>>>>>> d6ed5fa8a748f26693d5202d50c73a4cc2cb648f
 
 require_once 'classes/db.class.php';
 $db = new db();
