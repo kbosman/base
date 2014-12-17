37c1e884c04c25e400ba153f21d2019443f95993
kbosman <k.bosman35@gmail.com>
2014-12-17 12:55:22 +0100

    Create FAQ Klaar

--- classes/create.class.php
+++ classes/create.class.php
@@ -37,19 +37,41 @@ class create extends user {
 //</html>
 
 public function create_FAQ() {
+//Kevin Bosman
 $this->db_table = "MEDEWERKER";
 
 $fields = array("idMedewerker");
 $where = array("Gebruikersnaam" => $_SESSION['gebruikersnaam']);
 
 $idMedewerker = $this->select($fields, $where); 
-
-var_dump($idMedewerker);
-
+//Medewerker id from logged in user
 
 
+$this->db_table = "FAQ";
 
-//Vraag, Beschrijving, Oplossing , idMedewerker
+ $fields = array(
+	"Vraag",
+	"Beschrijving",
+	"Oplossing");
+	
+//Ophalen van de benodige velden
+
+	$data = array();
+        foreach ($fields as $key) {
+            $data[$key] = filter_input(INPUT_POST, $key);
+				if ($data[$key] === '') {
+					trigger_error("U heeft niets ingevuld!");
+				}
+		}
+//Vullen van de benodige velden
+			
+	$check = $this->insert($data);
+        if ($check === 1){
+            return TRUE;
+        } 	else{
+				trigger_error("Fout tijdens het aanmaken van uw FAQ");
+			}
+//velden naar database sturen
 
 
 }
