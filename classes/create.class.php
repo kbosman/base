f8a684f573ad71baa53d768d187a711451c207c5
kbosman <k.bosman35@gmail.com>
2014-12-17 18:47:33 +0100

    create status gemaakt

--- classes/create.class.php
+++ classes/create.class.php
@@ -6,8 +6,6 @@ class create extends user {
         parent::__construct();
     }
     
-
-
 public function FAQ() {
 //Kevin Bosman
 $this->db_table = "MEDEWERKER";
@@ -16,7 +14,7 @@ $fields = array("idMedewerker");
 $where = array("Gebruikersnaam" => $_SESSION['gebruikersnaam']);
 
 $idMedewerker = $this->select($fields, $where); 
-//↑Medewerker id from logged in user
+//↑Medewerker id van de ingelogde gebruiker
 
 
 $this->db_table = "FAQ";
@@ -86,8 +84,44 @@ $fields = array(
 
 
 
-public function create_status() {
+public function status() {
+//Kevin Bosman
 $this->db_table = "STATUS_WIJZIGING";
 
+ $fields = array(
+	"Status",
+	"SoortContact",
+	"Memo",
+	"DatumTijd"
+ );
+
+	$data = array();
+        foreach ($fields as $key) {
+            $data[$key] = filter_input(INPUT_POST, $key);
+				if ($data[$key] === '') {
+					trigger_error("U heeft niets ingevuld!");
+				}
+		}
+
+	$status = array('Nieuw','In behandeling','Doorgestuurd naar engineer','Doorgestuurd naar account manager','opgelost','afgemeld');
+			if(!in_array($data["Status"], $status)){
+				trigger_error("De invulmogelijkheden zijn: Nieuw, In behandeling, Doorgestuurd naar engineer, Doorgestuurd naar account manager, opgelost , afgemeld. Uw ingevulde incidenttype voldoet hier niet aan!");
+			}
+			
+			
+	date_default_timezone_set('Europe/Amsterdam');		
+	$date = date('Y-m-d H-i-s');
+	$data["DatumTijd"] = array("DatumTijd" => $date);
+	
+	
+	$check = $this->insert($data);
+        if ($check === 1){
+            return TRUE;
+        } 	else{
+				trigger_error("Fout tijdens het aanmaken van uw ticket");
+			}
+	
+	
 }
+
 }
